fa22c05f3387da81e86cb3d8ba78dd60bda84ed7
salmazobaa <zobaasalma@gmail.com>
2025-06-27 00:12:31 -0500

    added tuner class

--- Tuner.py
+++ Tuner.py
@@ -1,3 +1,24 @@
+from Vehicle import Car
 class Tuner:
-    def __init__(self, ):
-        pass
+    
+    def increase_horsepower(car: Car, amount: float):
+        new_horsepower = self.__horsepower + amount
+        if not isinstance (car, Car):
+            raise TypeError(f"{car} is not registered.")
+        assert amount >= 0, f"amount must be bigger than zero."
+        return f"horsepower is now {self.__horsepower}."
+    
+    def reduce_weight(car:Car, amount:float):
+        new_weight = self._weight - amount
+        if not isinstance (car, Car):
+            raise TypeError(f"{car} is not registered.")
+        assert amount >= 0, f"amount must be bigger than zero."
+        return f"weight is now {self._weight}"
+
+    def change_drivetrain(car:Car, new_drivetrain: str):
+        valid_drivetrains = ['RWD','AWD','FWD', '4WD']
+        if new_drivetrain not in valid_drivetrains:
+            print(f"{new_drivetrain}is not valid.")
+        else:
+            self.__drivetrain = new_drivetrain
+        return f"drivetrain is now {self.__drivetrain}."
